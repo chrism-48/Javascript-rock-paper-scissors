37789e3749c796b247618b5913b7e0b56b17107d
chrism-48 <moore4020@gmail.com>
2022-04-11 04:01:15 -0400

    changes in logic, still has problem

--- main.js
+++ main.js
@@ -5,48 +5,46 @@
 
 
 // -------------- initialize variables -------------------- //
+// computerSelection()
 
-
-let gameVar = ['rock', 'paper', 'scissors'];
-let user = document.getElementById("your_turn");
+let gamePick = ['rock', 'paper', 'scissors'];
+let user = document.getElementById("yourTurn");
 let computer = document.getElementById("computers_pick");
 let whoWins = document.getElementById("tell_winner");
 let resetButton = document.getElementById("clearAll").addEventListener("click", clearBoard );
-
-
+let submitButton = document.getElementById("btn1").addEventListener('click',playerSelection)
+// let listenToBox = document.getElementById('computers_pick').addEventListener('change', playRound(user, computer))
 // -------------------- get user input -------------------- //
 
 
 function playerSelection() {
+    // console.log(`You have chosen ${user.value.toLowerCase()}`);
+    computer.value = gamePick[Math.floor(Math.random() * gamePick.length)];
     computerSelection();
-    // console.log(`You have chosen ${user.value.toLowerCase()}.`);
-    user.value.toLowerCase()
+    // user.value.toLowerCase()
     // playRound(user, computer); 
     // setTimeout(function(){playRound(user, computer)},300);
-    return user.value;  
-    
-
+    return user.value, computer.value;  
 }
 
 // --------------- get computer random input ------------- //
 
 
 function computerSelection() {    
-    // gameVar = ['rock', 'paper', 'scissors'];
-    computer.value = gameVar[Math.floor(Math.random() * gameVar.length)];
-    // console.log(`Computer has chosen ${computer.value}.`);
+    // computer.value = gamePick[Math.floor(Math.random() * gamePick.length)];
+    // console.log(`Computer has chosen ${computer.value}`);
     // computer.value = computer.value;
-    // setTimer(function(){playRound(user, computer)},100);
+    // setTimer(function(){playRound(user, computer)},1000);
+    // setTimer(function(){return computer.value},1000);
     playRound(user, computer);
-    return computer.value;
-
-    
+    // return computer.value; 
 }
 
 
 // -------------- Find the winner of the game ------------- //
 
 function playRound(user, computer) {
+    console.log('Hello');
     
     
               
@@ -76,7 +74,7 @@ function playRound(user, computer) {
     } else if (user.value === 'scissors' && computer.value === 'scissors') {       
         whoWins.value = 'It is a tie!';
     } 
-    
+    console.log(user.value,computer.value);
 }
 
 // -------------- Clear all input boxes ------------- //
