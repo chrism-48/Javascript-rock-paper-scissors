cd4dd575da05df80a5bb8d33450e4a01bd118214
chrism-48 <moore4020@gmail.com>
2022-04-12 17:40:31 -0400

    added more input boxes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 
 
 // -------------- initialize variables -------------------- //
-// computerSelection()
+
 
 let gamePick = ['rock', 'paper', 'scissors'];
 let user = document.getElementById("yourTurn");
@@ -14,42 +14,29 @@ let computer = document.getElementById("computers_pick");
 let whoWins = document.getElementById("tell_winner");
 let resetButton = document.getElementById("clearAll").addEventListener("click", clearBoard );
 let submitButton = document.getElementById("btn1").addEventListener('click',playerSelection)
-// let listenToBox = document.getElementById('computers_pick').addEventListener('change', playRound(user, computer))
-// -------------------- get user input -------------------- //
 
 
-
+// ------------- get user and computer input -------------- //
 
 
 function playerSelection() {
-    // console.log(`You have chosen ${user.value.toLowerCase()}`);
     computer.value = gamePick[Math.floor(Math.random() * gamePick.length)];
     computerSelection();
-    // user.value.toLowerCase()
-    // playRound(user, computer); 
-    // setTimeout(function(){playRound(user, computer)},300);
     return user.value, computer.value;  
 }
 
-// --------------- get computer random input ------------- //
+// --------------- send to playRound function ------------- //
 
 
-function computerSelection() {    
-    // computer.value = gamePick[Math.floor(Math.random() * gamePick.length)];
-    // console.log(`Computer has chosen ${computer.value}`);
-    // computer.value = computer.value;
-    // setTimer(function(){playRound(user, computer)},1000);
-    // setTimer(function(){return computer.value},1000);
+function computerSelection() {       
     playRound(user, computer);
-    // return computer.value; 
+    
 }
 
 
 // -------------- Find the winner of the game ------------- //
 
-function playRound(user, computer) {
-   
-              
+function playRound(user, computer) {          
     if (user.value === 'rock' && computer.value === 'rock') {               
         whoWins.value = 'It is a tie!';
         console.log('it is a tie'); 
