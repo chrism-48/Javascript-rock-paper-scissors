84f5441bda910fc8872bac6b19a08004538fa38f
chrism-48 <moore4020@gmail.com>
2022-04-13 04:03:32 -0400

    added score boxes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 
 
-// -------------- initialize variables -------------------- //
+// -------------- declare variables -------------------- //
 
 
 let gamePick = ['rock', 'paper', 'scissors'];
@@ -14,7 +14,10 @@ let computer = document.getElementById("computers_pick");
 let whoWins = document.getElementById("tell_winner");
 let resetButton = document.getElementById("clearAll").addEventListener("click", clearBoard );
 let submitButton = document.getElementById("btn1").addEventListener('click',playerSelection)
-
+let myUserScore = document.getElementById('score1');
+let myComputerScore = document.getElementById('score2');
+let num1 = document.getElementById('score1').value = 0;
+let num2 = document.getElementById('score2').value = 0;
 
 // ------------- get user and computer input -------------- //
 
@@ -38,33 +41,39 @@ function computerSelection() {
 
 function playRound(user, computer) {          
     if (user.value === 'rock' && computer.value === 'rock') {               
-        whoWins.value = 'It is a tie!';
-        console.log('it is a tie'); 
+        whoWins.value = 'It is a tie!'; 
+
     } else if (user.value === 'rock' && computer.value === 'paper') {       
         whoWins.value = 'Paper covers rock, computer wins.';
+        myComputerScore.value = parseInt(num2) + parseInt(1);
 
     } else if (user.value === 'rock' && computer.value === 'scissors') {       
         whoWins.value = 'Rock breaks scissors, you win!';
+        myUserScore.value = parseInt(num1) + parseInt(1);
 
     } else if (user.value === 'paper' && computer.value === 'rock') {         
         whoWins.value = 'Paper covers rock, you win!';
+        myUserScore.value = parseInt(num1) + parseInt(1);
 
     } else if (user.value === 'paper' && computer.value === 'paper') {        
         whoWins.value = 'It is a tie!';
 
     } else if (user.value === 'paper' && computer.value === 'scissors') {         
         whoWins.value = 'Scissors cuts paper, computer wins.';
+        myComputerScore.value = parseInt(num2) + parseInt(1);
 
     } else if (user.value === 'scissors' && computer.value === 'rock') {        
         whoWins.value = 'Rock breaks scissors, computer wins.';
+        myComputerScore.value = parseInt(num2) + parseInt(1);
 
     } else if (user.value === 'scissors' && computer.value === 'paper') {         
         whoWins.value = 'Scissors cuts paper, you win!';
+        myUserScore.value = parseInt(num1) + parseInt(1);
 
     } else if (user.value === 'scissors' && computer.value === 'scissors') {       
         whoWins.value = 'It is a tie!';
     } 
-    console.log(user.value,computer.value);
+    
 }
 
 // -------------- Clear all input boxes ------------- //
@@ -73,6 +82,8 @@ function clearBoard() {
     user.value = " ";
     computer.value = " ";
     whoWins.value = " ";
+    myUserScore.value = ' ';
+    myComputerScore.value = ' ';
     
 }
 
@@ -82,6 +93,8 @@ function init() {
     user.value = " ";
     computer.value = " ";
     whoWins.value = " ";
+    myUserScore.value = ' ';
+    myComputerScore.value = ' ';
 }
 
 window.onload = init;
